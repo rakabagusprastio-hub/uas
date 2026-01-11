7f2cadc16983ad89356975cb28bae67c71158611
haikalwicaksono2 <haikalwcksono2@gmail.com>
2026-01-11 22:23:49 +0700

    function inputan

--- UAS.py
+++ UAS.py
@@ -1,65 +1,39 @@
 def header_invoice():
-print("KRISHAND INDONESIA")
-print("Jakarta Selatan - Indonesia")
-print("------------------------------------------------------------------------------------------")
-print("                                 I N V O I C E                                            ")
-print("Kepada Yth.                                               No.             :FT./003/01/2008 ")
-print("PT ISM BOGOSARI FLOUR JAKARTA                             Tanggal         :30/01/2008      ")
-print("Jl. Raya Cilincing, Tanjung Priok                         Mata uang       :IDR             ")
-print("Jakarta Utara - 14110                                     No. PO          :PO-0001/01/2008 ")
-print("                                                          Tgl.PO          :25/01/2008      ")
-print("                                                          Tgl Jatuh Tempo :13/02/2008      ")
-print("------------------------------------------------------------------------------------------")
-nama_barang = []
-satuan = []
-kuantum = []
-harga_satuan = []
-jumlah = []
+    print("KRISHAND INDONESIA")
+    print("Jakarta Selatan - Indonesia")
+    print("------------------------------------------------------------------------------------------")
+    print("                                 I N V O I C E                                            ")
+    print("Kepada Yth.                                               No.             :FT./003/01/2008 ")
+    print("PT ISM BOGOSARI FLOUR JAKARTA                             Tanggal         :30/01/2008      ")
+    print("Jl. Raya Cilincing, Tanjung Priok                         Mata uang       :IDR             ")
+    print("Jakarta Utara - 14110                                     No. PO          :PO-0001/01/2008 ")
+    print("                                                          Tgl.PO          :25/01/2008      ")
+    print("                                                          Tgl Jatuh Tempo :13/02/2008      ")
+    print("------------------------------------------------------------------------------------------")
 
-ulang = int(input("Masukkan jumlah jenis barang : "))
+def input_data():
+    global nama_barang, satuan, kuantum, harga_satuan, jumlah, ulang
 
-for i in range(ulang):
-    print(f"\nBarang ke-{i+1}")
-    nama = input("Nama barang   : ")
-    sat = input("Satuan        : ")
-    qty = int(input("Kuantum       : "))
-    harga = int(input("Harga satuan  : "))
+    # LIST DATA
+    nama_barang = []
+    satuan = []
+    kuantum = []
+    harga_satuan = []
+    jumlah = []
 
-    total = qty * harga
+    ulang = int(input("Masukkan jumlah jenis barang : "))
 
-    nama_barang.append(nama)
-    satuan.append(sat)
-    kuantum.append(qty)
-    harga_satuan.append(harga)
-    jumlah.append(total)
+    for i in range(ulang):
+        print(f"\nBarang ke-{i+1}")
+        nama = input("Nama barang   : ")
+        sat = input("Satuan        : ")
+        qty = int(input("Kuantum       : "))
+        harga = int(input("Harga satuan  : "))
 
-print("No   Nama Barang        Satuan   Kuantum   Harga Satuan     Jumlah")
-print("------------------------------------------------------------------------------------------")
-# ================== CETAK ISI TABEL ==================
-sub_total = 0
-for i in range(ulang):
-    print(f"{i+1:<4} {nama_barang[i]:<18} {satuan[i]:<8} {kuantum[i]:<9} {harga_satuan[i]:<15} {jumlah[i]}")
-    sub_total += jumlah[i]
+        total = qty * harga
 
-print("------------------------------------------------------------------------------------------")
-
-# ================== HITUNG TOTAL ==================
-ppn = sub_total * 0.10
-grand_total = sub_total + ppn
-
-print(f"{'Sub Total':<80} {sub_total}")
-print(f"{'Discount':<80} 0")
-print(f"{'Total':<80} {sub_total}")
-print(f"{'PPN 10%':<80} {int(ppn)}")
-print(f"{'Grand Total':<80} {int(grand_total)}")
-
-print("------------------------------------------------------------------------------------------")
-print("Pembayaran mohon ditransfer ke rekening:")
-print("Bank BCA Cab. Sudirman")
-print("No. Rekening : 035-0123456")
-print("Atas Nama    : PT Krishand Indonesia\n")
-
-print("Hormat kami,")
-print("\n\n( Vonny Kusuma )")
-print("Manager Accounting")
-print("------------------------------------------------------------------------------------------")
+        nama_barang.append(nama)
+        satuan.append(sat)
+        kuantum.append(qty)
+        harga_satuan.append(harga)
+        jumlah.append(total)
